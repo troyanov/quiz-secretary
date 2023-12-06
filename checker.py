f67eb63ef7aff194cb313ba0240efe4ab475e2e0
Anton Troyanov <anton@troyanov.net>
2023-12-06 13:52:14 +0300

    refactor: update url filter to check more venues

--- checker.py
+++ checker.py
@@ -8,7 +8,7 @@ chat_id = os.environ["CHAT_ID"]
 data_file = os.environ["DATA_FILE"]
 
 # Change URL for a different city or game types
-url = "https://quizplease.ru/schedule?QpGameSearch%5BcityId%5D=160&QpGameSearch%5Bdates%5D=&QpGameSearch%5Bstatus%5D%5B%5D=1&QpGameSearch%5Btype%5D%5B%5D=all&QpGameSearch%5Btype%5D%5B%5D=1&QpGameSearch%5Btype%5D%5B%5D=3&QpGameSearch%5Btype%5D%5B%5D=5&QpGameSearch%5Btype%5D%5B%5D=9&QpGameSearch%5Bbars%5D%5B%5D=1231"
+url = "https://tbilisi.quizplease.ru/schedule?QpGameSearch%5BcityId%5D=160&QpGameSearch%5Bdates%5D=&QpGameSearch%5Bstatus%5D%5B0%5D=1&QpGameSearch%5Btype%5D%5B0%5D=all&QpGameSearch%5Btype%5D%5B1%5D=1&QpGameSearch%5Btype%5D%5B2%5D=3&QpGameSearch%5Btype%5D%5B3%5D=5&QpGameSearch%5Btype%5D%5B4%5D=9&QpGameSearch%5Bbars%5D%5B0%5D=all&QpGameSearch%5Bbars%5D%5B1%5D=1231&QpGameSearch%5Bbars%5D%5B2%5D=1453"
 
 r = requests.get(url)
 
@@ -31,7 +31,7 @@ with open(filename, "r+") as f:
     for game in games:
         if game in lines:
             continue
-        msg = f"ЗАПИШИСЬ НА ИГРУ\nhttps://quizplease.ru/game-page?id={game}"
+        msg = f"ЗАПИШИСЬ БЛЯТЬ НА ИГРУ!\nhttps://quizplease.ru/game-page?id={game}"
         print("Game found: https://quizplease.ru/game-page?id={game}")
         r = requests.post(
             url=f"https://api.telegram.org/bot{token}/sendMessage",
